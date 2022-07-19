1924df8e85864c2d6e03e0e789d9f89d2bdeb84b
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-07-19 14:35:26 -0700

    rename options to option

--- views/fragments/header.php
+++ views/fragments/header.php
@@ -1,7 +1,7 @@
 <header class="hero" data-hero="home">
     <?php 
-    $bgHdr = get_field( 'bg_header', 'options' ); 
-    $bgContent = get_field( 'bg_content', 'options' ); ?>
+    $bgHdr = get_field( 'bg_header', 'option' ); 
+    $bgContent = get_field( 'bg_content', 'option' ); ?>
     <div class="uk-width-1-1@s uk-width-2-3@m uk-light uk-position-z-index | hero-container" data-src="<?php echo $bgHdr['url']; ?>" uk-img>
         <?php $bgContent; ?>
     </div>
