93675ab0bc6c343fc5c2806abdb476fd39dd8ecb
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-07-19 14:39:32 -0700

    fix header issue

--- views/fragments/header.php
+++ views/fragments/header.php
@@ -2,8 +2,8 @@
     <?php 
     $bgHdr = get_field( 'bg_header', 'option' ); 
     $bgContent = get_field( 'bg_content', 'option' ); ?>
-    <div class="uk-width-1-1@s uk-width-2-3@m uk-light uk-position-z-index | hero-container" data-src="<?php echo $bgHdr['url']; ?>" uk-img>
-        <?php $bgContent; ?>
+    <div class="uk-width-1-1@s uk-width-2-3@m uk-light uk-background-cover uk-position-z-index | hero-container" data-src="<?php echo $bgHdr['url']; ?>" uk-img>
+        <?php echo $bgContent; ?>
     </div>
 
     <div class="uk-overlay-primary uk-position-cover"></div>
