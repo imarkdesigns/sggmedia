78d6a439d57c452117a597e470ab8817795362ea
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-07-19 14:33:13 -0700

    fix wrong variable in header

--- views/fragments/header.php
+++ views/fragments/header.php
@@ -2,7 +2,7 @@
     <?php 
     $bgHdr = get_field( 'bg_header', 'options' ); 
     $bgContent = get_field( 'bg_content', 'options' ); ?>
-    <div class="uk-width-1-1@s uk-width-2-3@m uk-light uk-position-z-index | hero-container" data-src="<?php echo $bg_header['url']; ?>" uk-img>
+    <div class="uk-width-1-1@s uk-width-2-3@m uk-light uk-position-z-index | hero-container" data-src="<?php echo $bgHdr['url']; ?>" uk-img>
         <?php $bgContent; ?>
     </div>
 
