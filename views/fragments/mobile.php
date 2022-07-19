8abfe7280067df4e58af7290e46a5c43e8330169
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-07-19 14:42:20 -0700

    fix wrong permalink target url

--- views/fragments/mobile.php
+++ views/fragments/mobile.php
@@ -6,8 +6,8 @@
         </figure>
         <ul class="uk-nav-default uk-nav-parent-icon uk-nav-mobile uk-nav" uk-nav="">
             <li><a href="<?php echo esc_url( home_url() ); ?>">Home</a></li>
-            <li><a href="<?php echo esc_url( get_permalink( 56 ) ); ?>">Press Releases</a></li>
-            <li><a href="<?php echo esc_url( get_permalink( 58 ) ); ?>">Careers</a></li>
+            <li><a href="<?php echo esc_url( get_permalink( 58 ) ); ?>">Press Releases</a></li>
+            <li><a href="<?php echo esc_url( get_permalink( 56 ) ); ?>">Careers</a></li>
             <li><a href="<?php echo esc_url( get_permalink( 108 ) ); ?>">Contact</a></li>
             <li><a href="<?php echo esc_url( get_permalink( 3 ) ); ?>">Privacy Policy</a></li>
         </ul>
