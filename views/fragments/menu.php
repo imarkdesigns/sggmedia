31dde4a930a0d5638ee542dc8605d2e842de7a58
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-08-09 03:24:27 -0700

    update menu with social media links

--- views/fragments/menu.php
+++ views/fragments/menu.php
@@ -12,13 +12,6 @@ $logo         = wp_get_attachment_image_src( $customLogoID, 'full' ); ?>
                 <?php echo '<img src="'. $logo[0] .'" alt="'. get_bloginfo() .'">'; ?>
             </a>
         </div>
-        <div class="uk-navbar-center" hidden>
-            <ul class="uk-subnav">
-                <li class="icon-twitter"><a href="https://twitter.com/SGG_HQ" target="_blank" aria-label="Twitter"> <span uk-icon="icon: twitter"></span> </a></li>
-                <li class="icon-instagram"><a href="https://www.instagram.com/sgg_hq/" target="_blank" aria-label="Instagram"> <span uk-icon="icon: instagram"></span> </a></li>
-                <li class="icon-tiktok"><a href="https://www.tiktok.com/@sgghq" target="_blank" aria-label="TikTok"> <span uk-icon="icon: tiktok"></span> </a></li>
-            </ul>
-        </div>
         <div class="uk-navbar-right">
             <ul class="uk-navbar-nav uk-visible@l">
                 <li><a href="<?php echo esc_url( home_url() ); ?>">Home</a></li>
@@ -34,6 +27,11 @@ $logo         = wp_get_attachment_image_src( $customLogoID, 'full' ); ?>
                 <li><a href="<?php echo esc_url( get_permalink( 124 ) ); ?>">Our Team</a></li>
                 <li><a href="<?php echo esc_url( get_permalink( 58 ) ); ?>">Press</a></li>
                 <li><a href="<?php echo esc_url( get_permalink( 108 ) ); ?>">Contact Us</a></li>
+            </ul>
+            <ul class="uk-subnav uk-margin-remove uk-visible@l">
+                <li class="icon-twitter"><a href="https://twitter.com/SGG_HQ" target="_blank" aria-label="Twitter"> <span uk-icon="icon: twitter"></span> </a></li>
+                <li class="icon-instagram"><a href="https://www.instagram.com/sgg_hq/" target="_blank" aria-label="Instagram"> <span uk-icon="icon: instagram"></span> </a></li>
+                <li class="icon-tiktok"><a href="https://www.tiktok.com/@sgghq" target="_blank" aria-label="TikTok"> <span uk-icon="icon: tiktok"></span> </a></li>
             </ul>
             <button type="button" role="button" class="uk-navbar-toggle uk-hidden@l" uk-navbar-toggle-icon uk-toggle="target: #mobile" aria-label="Toggle Mobile Menu"></button>
         </div>
