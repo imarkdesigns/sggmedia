ee992383876db9336224edcacfd5265c72339ee7
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-08-17 13:00:48 -0700

    restore menu nav for other pages except registration

--- views/fragments/menu.php
+++ views/fragments/menu.php
@@ -8,17 +8,24 @@ $logo         = wp_get_attachment_image_src( $customLogoID, 'full' ); ?>
 
     <nav uk-navbar class="uk-navbar-container uk-navbar-transparent">
         <div class="uk-navbar-left">
-            <a href="#" class="uk-logo">
-                <?php if ( is_front_page() ) {
-                    echo '<img src="'. $logo[0] .'" alt="'. get_bloginfo() .'">';
-                } else {
-                    // echo '<img src="'._uri.'/resources/images/logo-sgg-media-black.png" alt="'.get_bloginfo().'">';
-                    echo '<img src="'. $logo[0] .'" alt="'. get_bloginfo() .'">';
-                } ?>
-            </a>
+            <?php if ( is_front_page() ) {
+                echo '<a href="'.esc_url( home_url() ).'" class="uk-logo">';
+                echo '<img src="'. $logo[0] .'" alt="'. get_bloginfo() .'">';
+                echo '</a>';
+            } elseif ( is_page([ 302 ]) ) {
+                echo '<a class="uk-logo" style="display:flex;align-items:center;height:80px;">';
+                echo '<img src="'. $logo[0] .'" alt="'. get_bloginfo() .'" style="height:60px;">';
+                echo '</a>';
+            } else {
+                echo '<a href="'.esc_url( home_url() ).'" class="uk-logo">';
+                // echo '<img src="'._uri.'/resources/images/logo-sgg-media-black.png" alt="'.get_bloginfo().'">';
+                echo '<img src="'. $logo[0] .'" alt="'. get_bloginfo() .'">';
+                echo '</a>';
+            } ?>
         </div>
+        <?php if ( !is_page([ 302 ]) ) : ?>
         <div class="uk-navbar-right">
-            <ul class="uk-navbar-nav uk-visible@l" hidden>
+            <ul class="uk-navbar-nav uk-visible@l">
                 <li><a href="<?php echo esc_url( home_url() ); ?>">Home</a></li>
                 <?php if ( is_front_page() ) : ?>
                 <li><a uk-scroll="offset: 70" href="<?php echo esc_url( site_url('#about-us') ); ?>">About Us</a></li>
@@ -40,13 +47,14 @@ $logo         = wp_get_attachment_image_src( $customLogoID, 'full' ); ?>
                 <li><a href="<?php echo esc_url( get_permalink( 58 ) ); ?>">Press</a></li>
                 <li><a href="<?php echo esc_url( get_permalink( 108 ) ); ?>">Contact Us</a></li>
             </ul>
-            <ul class="uk-subnav uk-margin-remove uk-visible@l" hidden>
+            <ul class="uk-subnav uk-margin-remove uk-visible@l">
                 <li class="icon-twitter"><a href="https://twitter.com/SGG_HQ" target="_blank" aria-label="Twitter"> <span uk-icon="icon: twitter"></span> </a></li>
                 <li class="icon-instagram"><a href="https://www.instagram.com/sgg_hq/" target="_blank" aria-label="Instagram"> <span uk-icon="icon: instagram"></span> </a></li>
                 <li class="icon-tiktok"><a href="https://www.tiktok.com/@sgghq" target="_blank" aria-label="TikTok"> <span uk-icon="icon: tiktok"></span> </a></li>
             </ul>
-            <button type="button" role="button" class="uk-navbar-toggle uk-hidden@l" uk-navbar-toggle-icon uk-toggle="target: #mobile" aria-label="Toggle Mobile Menu" hidden></button>
+            <button type="button" role="button" class="uk-navbar-toggle uk-hidden@l" uk-navbar-toggle-icon uk-toggle="target: #mobile" aria-label="Toggle Mobile Menu"></button>
         </div>
+        <?php endif; ?>
     </nav>
 
 </div>
