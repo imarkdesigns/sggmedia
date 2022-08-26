56704648fb851bdf9098b0795f5b6ef15ad8fd1a
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2022-08-26 11:38:42 -0700

    create cookie to reset counter/ticker

--- resources/scripts/route/common.js
+++ resources/scripts/route/common.js
@@ -23,70 +23,100 @@
 
     });
 
+
+
 })(jQuery);
 
-// total count-up for Followers/Impressions
-var followers = document.getElementById('total-followers');
-var influencers = document.getElementById('total-influencers');
-var impressions = document.getElementById('total-impressions');
 
-function updateInfluencers() {
-    let currentInfluencers = influencers.dataset.influencers;
-    influencers.innerHTML = currentInfluencers.toLocaleString('en-US');
-    // console.log(currentInfluencers);
-}
-updateInfluencers()
+jQuery.getScript('https://cdn.jsdelivr.net/npm/js-cookie@2.2.1/src/js.cookie.min.js', function() {
+    Cookies.set('_ufv', 'active', { expires: 30 })
 
-function updateFollowers() {
-    let currentFollowers = followers.dataset.followers;
-    let currentTicker = localStorage.getItem( 'followers' );
 
-    if ( currentFollowers > currentTicker ) {
-        let tickerFollowers = setInterval(function() {
-            let tickerCounter = Math.floor(currentFollowers++);
-            followers.classList.toggle("animate__flash");
-            followers.innerHTML = tickerCounter.toLocaleString();
+    // total count-up for Followers/Impressions
+    var followers = document.getElementById('total-followers');
+    var influencers = document.getElementById('total-influencers');
+    var impressions = document.getElementById('total-impressions');
 
-            localStorage.setItem('followers', tickerCounter);
-        }, 1000);
-    } else {
-        let tickerFollowers = setInterval(function() {
-            followers.classList.toggle("animate__flash");
-            let tickerCounter = Math.floor(currentTicker++);
-            followers.innerHTML = tickerCounter.toLocaleString();
-
-            localStorage.removeItem( 'followers' );
-            localStorage.setItem('followers', tickerCounter);
-        }, 3000);
+    function updateInfluencers() {
+        let currentInfluencers = influencers.dataset.influencers;
+        influencers.innerHTML = currentInfluencers.toLocaleString('en-US');
+        // console.log(currentInfluencers);
     }
-}
-updateFollowers();
+    updateInfluencers()
 
+    function updateFollowers() {
+        let currentFollowers = followers.dataset.followers;
 
-function updateImpressions() {
-    let currentImpressions = impressions.dataset.impressions;
-    let currentTicker = localStorage.getItem( 'impressions' );
+        // Reset the Followers base on active Cookie
+        if ( Cookies.get('_ufv') ) {
+            // console.log('cookie is set');
+            localStorage.setItem( 'followers', currentFollowers );
+            var currentTicker = localStorage.getItem( 'followers' );
+        } else {
+            // console.log('cookie is not set');
+        }
 
-    if ( currentImpressions > currentTicker ) {
-        let tickerImpressions = setInterval(function() {
-            let tickerCounter = Math.floor(currentImpressions++);
-            impressions.classList.toggle("animate__flash");
-            impressions.innerHTML = tickerCounter.toLocaleString();
+        if ( currentFollowers > currentTicker ) {
+            console.log('Followers: First Run');
+            let tickerFollowers = setInterval(function() {
+                let tickerCounter = Math.floor(currentFollowers++);
+                followers.classList.toggle("animate__flash");
+                followers.innerHTML = tickerCounter.toLocaleString();
 
-            localStorage.setItem('impressions', tickerCounter);
-        }, 1000);
-    } else {
-        let tickerImpressions = setInterval(function() {
-            let tickerCounter = Math.floor(currentTicker++);
-            impressions.classList.toggle("animate__flash");
-            impressions.innerHTML = tickerCounter.toLocaleString();
+                localStorage.setItem('followers', tickerCounter);
+            }, 3000);
+        } else {
+            console.log('Followers: Continuous Run');
+            let tickerFollowers = setInterval(function() {
+                followers.classList.toggle("animate__flash");
+                let tickerCounter = Math.floor(currentTicker++);
+                followers.innerHTML = tickerCounter.toLocaleString();
 
-            localStorage.removeItem( 'impressions' );
-            localStorage.setItem('impressions', tickerCounter);
-        }, 5000);
+                localStorage.removeItem( 'followers' );
+                localStorage.setItem('followers', tickerCounter);
+            }, 3000);
+        }
     }
-}
-updateImpressions();
+    updateFollowers();
+
+
+    function updateImpressions() {
+        let currentImpressions = impressions.dataset.impressions;
+
+        // Reset the Followers base on active Cookie
+        if ( Cookies.get('_ufv') ) {
+            // console.log('cookie is set');
+            localStorage.setItem( 'impressions', currentImpressions );
+            var currentTicker = localStorage.getItem( 'impressions' );
+        } else {
+            // console.log('cookie is not set');
+        }        
+        
+        if ( currentImpressions > currentTicker ) {
+            console.log('Impressions: First Run');
+            let tickerImpressions = setInterval(function() {
+                let tickerCounter = Math.floor(currentImpressions++);
+                impressions.classList.toggle("animate__flash");
+                impressions.innerHTML = tickerCounter.toLocaleString();
+
+                localStorage.setItem('impressions', tickerCounter);
+            }, 1000);
+        } else {
+            console.log('Impressions: Continuous Run');
+            let tickerImpressions = setInterval(function() {
+                let tickerCounter = Math.floor(currentTicker++);
+                impressions.classList.toggle("animate__flash");
+                impressions.innerHTML = tickerCounter.toLocaleString();
+
+                localStorage.removeItem( 'impressions' );
+                localStorage.setItem('impressions', tickerCounter);
+            }, 5000);
+        }
+    }
+    updateImpressions();
+
+});
+
 
 // The basic check of site fully loaded
 if(document.readyState === 'complete') {
