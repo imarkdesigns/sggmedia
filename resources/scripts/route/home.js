3a0d3c2bbdea8a86e80d500c0e47ac2d5c67a67a
Oh Wow Marketing Solutions <imarkdesigns@gmail.com>
2023-11-21 15:32:48 -0800

    update page

--- resources/scripts/route/home.js
+++ resources/scripts/route/home.js
@@ -20,11 +20,9 @@ new fullpage('#sgg-section', {
     autoScrolling: true,
 
     // License
-    licenseKey: 'L9LU6-5S46I-W3K78-TJW8I-BWAKO',
     
     // Extension
     // scrollHorizontally: true,
-    // scrollHorizontallyKey: '3270A37C-9B454A7E-92FCDA4B-C1B75168',
 
     // Options
     menu: '#main-menu',
